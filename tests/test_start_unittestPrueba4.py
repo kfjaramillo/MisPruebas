eacf7c4ff0d36dcbc66fd2364e1e5c4e6a1afc77
Karina <kfjaramillo.puce.edu.ec>
2024-07-22 01:36:13 -0500

    Prueba4

--- tests/test_start_unittestPrueba4.py
+++ tests/test_start_unittestPrueba4.py
@@ -1,41 +1,36 @@
-#convertir a unittest
 import unittest
-#importar librerías: webdriver(librería principal para controlar el navegador) y by (localización de elementos en la web ID, nombre, clase,..)
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# verificar la prueba en tiempo
 import time
-#heredar de unittest clase
-class ClickAndSendKeys(unittest.TestCase):
-    # crear 2 funciones Python inicio y fin de pruebas
-    # metodo de inicio de prueba. Usar driver de Selenium en navegador 
-    # self es una variable 
-    def setUp(self):
 
-    #crear una nueva instancia del navegador Microsoft Edge
+
+class ClickAndSendKeys(unittest.TestCase):
+
+    def setUp(self):
+    
         self.driver = webdriver.Edge() 
 
-    #navegar a una web
-        self.driver.get("https://kfjaramillo.github.io/PaginaPruebas/")
+        self.driver.get("https://kfjaramillo.github.io/MisPruebas")
 
-    #Agregar un método las validaciones realizadas 
     def testId(self):
-        #busca un elemento en la página, en este caso la clase rojo
         liga = self.driver.find_element(By.XPATH , "//a[contains(text(),'Pagina 2')]")
 
-        #verifica si el elemento fue encontrado y al dar click se va a la 2da pagina
         if liga is not None:
             liga.click()
-        # encontrar por ID   
-            nombre = self.driver.find_element(By.ID,"segundo")
-   
-        # validr si fue encontrado y escribir utilizando la funcion send_keys
+
+
+        nombre = self.driver.find_element(By.ID, "Segundo")
+
         if nombre is not None:
             nombre.send_keys("Karina")
-        #tiempo de espera
-        time.sleep(5)  
-    # Metodo para cerrar el driver 
-    def tearDown(self):
-        #cierra el navegador  
+
+        time.sleep(5)
+
+
+
+            
+    def tearDown(self):    
         self.driver.quit()
-        
+
+if __name__ == "__main__":
+    unittest.main()
